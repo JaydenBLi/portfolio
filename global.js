bd2427b4fdf02b3bdd2029682dd248ac9abe4e16
JaydenBLi <jbl011@ucsd.edu>
2026-04-16 15:57:57 -0700

    color scheme selector

--- global.js
+++ global.js
@@ -43,3 +43,25 @@ for (let p of pages) {
 
 }
 
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+			<option value="light dark" selected>Automatic</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+		</select>
+	</label>`,
+);
+let select=document.querySelector('.color-scheme select');
+select.addEventListener('input', function (event) {
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value
+});
+if ("colorScheme" in localStorage){
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value=localStorage.colorScheme;
+}
+
